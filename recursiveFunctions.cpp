bd57ea045a76b45cb764321260ba957c6f4a925c
Alena Kozlova <alena.kozlova2004@mail.ru>
2023-10-25 01:03:57 +0300

    add function for place queens

--- recursiveFunctions.cpp
+++ recursiveFunctions.cpp
@@ -20,13 +20,21 @@ int moveKnight(int, int, int, int, int,
 void startMoveKnight();
 void printBoard(int**, int);
 
+void startPlaceQueens();
+int check(int, int, vector<int>&);
+void placeQueens(int, int, vector<int>&, vector<vector<int>>&);
+void printResult(int, vector<vector<int>>&);
+
+
 int main()
 {
-	startFindMaxElem();
+	//startFindMaxElem();
 
-	startHanoiTowers();
+	//startHanoiTowers();
 
-	startMoveKnight();
+	//startMoveKnight();
+
+	startPlaceQueens();
 }
 
 
@@ -212,6 +220,8 @@ void printBoard(int**board, int size) {
 
 void startMoveKnight() {
 
+	cout << "the movement of the knight on the board through recursion" << endl << endl;
+
 	int setMovesX[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
 	int setMovesY[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
@@ -289,4 +299,125 @@ int moveKnight(int i, int xPos, int yPos, int sizeBoard, int countSteps,
 		}
 	}
 	return q;
+}
+
+
+void printResult(int size, vector<int>& result) {
+
+	vector<vector<int>> matrixBoard;
+	for (int i = 0; i < size; i++)
+	{
+		vector<int> temp;
+		for (int j = 0; j < size; j++)
+		{
+			temp.push_back(0);
+		}
+		matrixBoard.push_back(temp);
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << endl;
+	cout << endl;
+	for (int i = 0; i < size; i++)
+	{
+		matrixBoard[i][result[i]] = 1;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cout << matrixBoard[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void startPlaceQueens() {
+
+	cout << "recursive placement of disjoint queens" << endl << endl;
+
+	vector<vector<int>> results;
+	int size;
+	cout << "Enter size of board ";
+	cin >> size;
+	vector<int> board(size);
+	for (int i = 0; i < size; i++)
+	{
+		board[i] = -1;
+	}
+
+	placeQueens(size, 0, board, results);
+
+	cout << "Print all results? y/n ";
+	char userAnswer;
+	cin >> userAnswer;
+	if (userAnswer == 'y')
+	{
+		for (int i = 0; i < results.size(); i++)
+		{
+			printResult(size, results[i]);
+		}
+	}
+	else if (userAnswer == 'n')
+	{
+		cout << "Print last result" << endl;
+		printResult(size, results[results.size() - 1]);
+	}
+	else
+	{
+		cout << "Invalid input" << endl;
+	}
+}
+
+int check(int row, int column, vector<int>& board) {
+
+	for (int i = 0; i < row; i++)
+	{
+		if (board[i] == column || board[i] - i == column - row ||
+			board[i] + i == column + row)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void placeQueens(int size, int row, vector<int>& board, vector<vector<int>>& results) {
+
+	if (row == size) 
+	{
+		results.push_back(board);
+		return;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		if (check(row, i, board))
+		{
+			board[row] = i;
+
+			cout << "Queen place on " << row << " position" << endl;
+			for (int i = 0; i < board.size(); i++)
+			{
+				cout << board[i] << " ";
+			}
+			cout << endl;
+
+			placeQueens(size, row + 1, board, results);
+
+			board[row] = -1;
+
+			cout << "Queen delete from" << row << " position" << endl;
+			for (int i = 0; i < board.size(); i++)
+			{
+				cout << board[i] << " ";
+			}
+			cout << endl;
+		}
+	}
 }
